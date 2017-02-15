b65d213ffb602053fd12a4f19ca61b116a9273ca
Jeremiah <jeremiah@ideonic.com>
2017-02-15 17:09:06 +0800

    cleans up example

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# Single User Node Authentication Example
+# Express API Key Authentication Example
 
-A single auth-token is stored in the environment variables. 
-For secret routes, it is compared against a second token passed in the http header.
-The AJAX example retrieves the token from local storage.
+This example shows a simple way to secure an API, you a single API Key, which must be given in all requests. This API Key can be stored in the server's environment variables and compared against a second token passed in the request http header.
 
+In this example, the AJAX code retrieves the token from local storage. In the same way, if you wanted an easy way to add Admin access to your API, you could manually add this token to the local storage on your browser, then only you would be able to access the Admin features of the API.
 
-You can use something like: http://randomkeygen.com/ to generate a random key and store it in your environment vars.
-You can then add it to your local storage so that only you can access the secret features.
+You can use something like: http://randomkeygen.com/ to generate a random key.
---
+++ index.js
@@ -0,0 +1,42 @@
+// load in the environment vars
+require('dotenv').config({silent: true})
+
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+// enable CORS for all routes and for our specific API-Key header
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key')
+  next()
+})
+
+// ROUTES
+// serve everything in assets folder as static etc
+app.use(express.static('public'))
+
+// unprotected root route
+app.get('/normal', (req, res) => {
+  res.status(200).json({message: 'This is NOT secret information'})
+})
+
+// PROTECT ALL ROUTES THAT FOLLOW
+app.use((req, res, next) => {
+  const apiKey = req.get('API-Key')
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    res.status(401).json({error: 'unauthorised'})
+  } else {
+    next()
+  }
+})
+
+// secret routes
+app.get('/secret', (req, res) => {
+  res.status(200).json({message: 'This is secret information'})
+})
+
+// START SERVER
+app.listen(process.env.PORT, () => {
+  console.log(`Server Listening on port ${process.env.PORT}`)
+})
